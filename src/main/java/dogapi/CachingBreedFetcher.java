ba4250f56d55392f44efad3a6edad1a0a53595f5
xande <xandersaiki@gmail.com>
2025-10-17 11:22:38 -0400

    Completed Tasks 3 and 4

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -24,7 +24,7 @@ public class CachingBreedFetcher implements BreedFetcher {
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
         // return statement included so that the starter code can compile and run.
         List<String> subBreeds = new ArrayList<>();
         if (cache.containsKey(breed)){
--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -1,10 +1,17 @@
 package dogapi;
 
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BreedFetcher.BreedNotFoundException {
         String breed = "hound";
         BreedFetcher breedFetcher = new CachingBreedFetcher(new BreedFetcherForLocalTesting());
         int result = getNumberOfSubBreeds(breed, breedFetcher);
@@ -23,9 +30,16 @@ public class Main {
      * @return the number of sub breeds. Zero should be returned if there are no sub breeds
      * returned by the fetcher
      */
-    public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
+    public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher){
         // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
+        int count = 0;
+        try{
+            List<String> subBreeds = breedFetcher.getSubBreeds(breed);
+            for (String subBreed : subBreeds) {
+                count++;
+            }
+        }catch(BreedFetcher.BreedNotFoundException e){}
         // return statement included so that the starter code can compile and run.
-        return -1;
+        return count;
     }
 }
